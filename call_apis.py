b52c4a8fae3473756c2a0db39468627cfafdab9d
enik <eevanikkari@gmail.com>
2019-02-01 15:13:51 +0200

    Randomize subreddit of the day

--- call_apis.py
+++ call_apis.py
@@ -6,21 +6,22 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 
 
-def get_r_aww(user_agent=""):
-    url = "https://www.reddit.com/r/aww/top.json"
+def get_subreddit_top_post(subreddit="aww", user_agent=""):
+    url = f"https://www.reddit.com/r/{subreddit}/top.json"
 
     response = requests.get(url, headers={"User-Agent": user_agent})
 
-    return response.json()["data"]["children"][0]["data"]
+    resp_data = response.json()["data"]["children"][0]["data"]
+    return {'url': f"https://www.reddit.com/{resp_data['permalink']}", 'title': resp_data['title']}
 
 
-def post_to_flowdock(flowdock_token, flow_token, bot_name, message):
+def post_to_flowdock(flowdock_token, flow_token, bot_name, message, tag="productivity"):
     url = f"https://{flowdock_token}@api.flowdock.com/messages/chat/{flow_token}"
 
     payload = \
         {"external_user_name": bot_name,
          "content": f"{message}",
-         "tags": "productivity"}
+         "tags": tag}
     headers = {
         'content-type': "application/json"
     }
--- cutie_bot.py
+++ cutie_bot.py
@@ -1,20 +1,35 @@
-from call_apis import get_r_aww, post_to_flowdock
+from call_apis import get_subreddit_top_post, post_to_flowdock
 import logging
+import random
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s %(name)s:%(lineno)d - %(message)s")
 logger = logging.getLogger(__name__)
 
+subreddits = ["RarePuppers",
+              "SupermodelCats",
+              "awwducational",
+              "corgi",
+              "PartyParrot",
+              "TheCatTrapIsWorking",
+              "Catloaf",
+              "eyebleach",
+              "Otters"
+              ]
+
 
 def send_message(flowdock_token, flow_token, bot_name="cutiebot"):
     try:
-        result = get_r_aww(user_agent=bot_name)
+        subreddit = random.choice(subreddits)
+        result = get_subreddit_top_post(subreddit=subreddit,
+                                        user_agent=bot_name)
         title = result.get('title')
         link = result.get('url')
-        message = f'Look at this cutie:\n "{title}"\n {link}'
+        message = f"Look at this cutie:\n \"{title}\"\n {link}"
 
         post_to_flowdock(flowdock_token,
                          flow_token,
                          bot_name,
-                         message)
+                         message,
+                         tag=subreddit)
     except BaseException as e:
         logger.info(e)
